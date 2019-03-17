b95196f8eaa82a2d3b22b3b51b974d0849701ce5
Jack Hessel <jmhessel@gmail.com>
2019-03-17 18:37:02 -0400

    added (almost) arbitrary window sizes, multiple comparisons, etc

--- count_motifs.py
+++ count_motifs.py
@@ -7,6 +7,7 @@ import numpy as np
 import scipy.stats
 import tqdm
 import prettytable
+import statsmodels.stats.multitest
 
 
 def parse_args():
@@ -25,11 +26,11 @@ def parse_args():
                         help='What is the character set?')
     parser.add_argument('--prior',
                         type=float,
-                        default=100.,
+                        default=10.,
                         help='How strong should the dirichlet prior be?')
     parser.add_argument('--pval',
                         type=float,
-                        default=.0001,
+                        default=.01,
                         help='What is the p-value for a true difference?')
     return parser.parse_args()
 
@@ -68,60 +69,79 @@ def z_score_to_pval(score):
 
 
 def main():
-    args = parse_args()    
-    vocab = list(itertools.product(
-        args.character_vocabulary, repeat=args.window))
-    vocab = set([''.join(v) for v in vocab])
-    # make sure that the vocab size is what we expect
-    assert(len(vocab) == len(args.character_vocabulary)**args.window)
-    vocab2idx = {v:idx for idx, v in enumerate(sorted(list(vocab)))}
-
-    if len(vocab) > 10**6:
-        print('Warning: your vocab size is over 1M! Much larger and we might be slow...')
-    
-    print('There were {} motifs of size {}'.format(len(vocab), args.window))
+    args = parse_args()
     seqs1, seqs2 = map(load_sequences, [args.file1, args.file2])
     motifs1, motifs2 = map(lambda x: count_motifs(x, args.window), [seqs1, seqs2])
 
+    observed_vocab = set(list(motifs1.keys()) + list(motifs2.keys()))
+    valid_vocab = []
+    valid_chars = set(args.character_vocabulary)
+    print('Checking each vocab item to make sure it only consists of {}'.format(
+        args.character_vocabulary))
+    for v in tqdm.tqdm(observed_vocab):
+        if set(v).issubset(valid_chars):
+            valid_vocab.append(v)
+        else:
+            continue
+    
+    print('Vocab size: {}'.format(len(valid_vocab)))
     print('{}/{} motifs in seqs1/seqs2.'.format(sum(motifs1.values()),
                                                 sum(motifs2.values())))
 
-    count_matrix = np.zeros([2, len(vocab)], dtype=np.float32)
-    for v, idx in vocab2idx.items():
-        count_matrix[0, idx] = motifs1[v]
-        count_matrix[1, idx] = motifs2[v]
-
-    n1, n2 = np.sum(count_matrix, axis=1)
+    motifs1 = {v:motifs1[v] for v in valid_vocab}
+    motifs2 = {v:motifs2[v] for v in valid_vocab}
+    
+    n1, n2 = sum(motifs1.values()), sum(motifs2.values())
         
     results = []
-    prior, sum_prior = args.prior, len(vocab2idx) * args.prior
-    for v, idx in vocab2idx.items():
-        term1 = np.log((count_matrix[0, idx]+prior)/(n1+sum_prior-count_matrix[0, idx]-prior))
-        term2 = np.log((count_matrix[1, idx]+prior)/(n2+sum_prior-count_matrix[1, idx]-prior))
+    prior, sum_prior = args.prior, len(valid_vocab) * args.prior
+    print('Computing z-scores')
+    
+    for v in tqdm.tqdm(valid_vocab):
+        term1 = np.log((motifs1[v] + prior)/(n1 + sum_prior - motifs1[v] - prior))
+        term2 = np.log((motifs2[v] + prior)/(n2 + sum_prior - motifs2[v] - prior))
         delta = term1 - term2
-        var = 1./(count_matrix[0, idx] + prior) + 1./(count_matrix[1, idx] + prior)
+        var = 1./(motifs1[v] + prior) + 1./(motifs2[v] + prior)
         # motif, z-score, smoothed rate in 1, smoothed rate in 2
         results.append((v, delta/np.sqrt(var), np.exp(term1), np.exp(term2)))
 
-    sig_results = [r for r in results if z_score_to_pval(r[1]) < args.pval]
+    # vectorized p_value computation, and multiple comparison correction
+    z_scores = np.array([r[1] for r in results])
+    p_vals = z_score_to_pval(z_scores)
 
+    reject, p_val, _, _ = statsmodels.stats.multitest.multipletests(
+        p_vals, alpha=args.pval, method='holm')
+    sig_results = [res for res, rej in zip(results, reject) if rej]
+    print('{} significant results'.format(len(sig_results)))
+
+    row_count = 0
     table1 = prettytable.PrettyTable()
     table1.field_names = ['Motif', 'Z-score', 'Rate in seqs1', 'Rate in seqs2']
     print('More in {} sequences:'.format(args.file1))
     for r in sorted(sig_results, key=lambda x: -x[1]):
         if r[1] > 0:
+            row_count += 1
             row = [r[0], '{:.1f}'.format(r[1]), '{:.4f}'.format(r[2]), '{:.4f}'.format(r[3])]
             table1.add_row(row)
-    print(table1)
-
+    
+    if row_count == 0:
+        print('None.')
+    else:
+        print(table1)
+    
+    row_count = 0
     table2 = prettytable.PrettyTable()
     table2.field_names = ['Motif', 'Z-score', 'Rate in seqs1', 'Rate in seqs2']
     print('More in {} sequences:'.format(args.file2))
     for r in sorted(sig_results, key=lambda x: x[1]):
         if r[1] < 0:
+            row_count += 1
             row = [r[0], '{:.1f}'.format(r[1]), '{:.4f}'.format(r[2]), '{:.4f}'.format(r[3])]
             table2.add_row(row)
-    print(table2)
+    if row_count == 0:
+        print('None.')
+    else:
+        print(table2)
     
     
 if __name__ == '__main__':
